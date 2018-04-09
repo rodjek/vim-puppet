0d5e3d9e9c62f297d208c88c9f3e7c87d8e39f7b
Marek Maksimczyk <marek.maksimczyk@xstream.net>
2018-04-09 09:54:51 +0200

    Setup travis to test both editors: vim and neovim
    Based on vader.vim tavis configuration:
    https://github.com/junegunn/vader.vim/blob/master/.travis.yml
    
    This setup check plugin agains travis build version (7.x at this
    moment), and neovim nightly build version.

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 #
 # Download dependencies for tests and run full test suit
+# Can use TESTVIM env variable to choose between vim and nvim (neovim)
 #
+set -x
 SCRIPT_FOLDER=$(dirname "$(readlink -f "$0")")
 
-if [ -x "$(command -v vim)" ]; then
-  VIM=vim
-elif [ -x "$(command -v nvim)" ]; then
-  VIM=nvim
+# TESTVIM env variable has precedence over installed programs
+# next in row is neovim and last one is vim
+if [ "$TESTVIM" == "vim" ]; then
+  RUNVIM=vim
+elif [ "$TESTVIM" == "nvim" ] || [ -x "$(command -v nvim)" ]; then
+  RUNVIM=nvim
   export VADER_OUTPUT_FILE=$(mktemp)
   trap "rm -f ${VADER_OUTPUT_FILE}" EXIT INT QUIT TERM
+elif [ -x "$(command -v vim)" ]; then
+  RUNVIM=vim
 else
   echo 'Error: vim is not installed.' >&2
   exit 1
@@ -21,7 +27,8 @@ if [ ! -d "vader.vim" ]; then
   git clone https://github.com/junegunn/vader.vim.git
 fi
 
-"${VIM}" -u test/init.vim -c 'Vader! test/**/*.vader' > /dev/null
+"${RUNVIM}" -u test/init.vim -c 'Vader! test/**/*.vader' > /dev/null
 vader_exit=$?
 [ -n "${VADER_OUTPUT_FILE}" ] && cat "${VADER_OUTPUT_FILE}"
 exit $vader_exit
+
